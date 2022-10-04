a2cfa8402597d96b3956d1213ad24ca6ce4156e4
Josquin Debaz <josquindebaz@protonmail.com>
2022-10-04 11:36:12 +0200

    https

--- scrappers/boxoffice.py
+++ scrappers/boxoffice.py
@@ -7,12 +7,15 @@ Scrap to retrieve box-office star movie
 import re
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context  # pylint: disable=protected-access
 
 
 def get():
     """ Get the box-office star movie """
 
-    url = "http://www.cine-directors.net/boxoff.htm"
+    url = "https://www.cine-directors.net/boxoff.htm"
     with urlopen(url) as request:
         soup = BeautifulSoup(request, "lxml")
     raw_value = soup.find_all("table")[3].find_all("tr")[2].find_all("td")[1].contents[1].string
--- scrappers/last_crash.py
+++ scrappers/last_crash.py
@@ -7,17 +7,20 @@ Scrap to retrieve last airplane crash
 import re
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context  # pylint: disable=protected-access
 
 
 def get():
     """ Get the last crash """
 
-    url = "http://www.planecrashinfo.com/database.htm"
+    url = "https://www.planecrashinfo.com/database.htm"
     with urlopen(url) as request:
         soup = BeautifulSoup(request, "lxml")
     url = list(filter(lambda x: re.search(r"\d{4}\.htm", x),
                       [link.get("href") for link in soup.find_all("a")]))[-1]
-    url = f"http://www.planecrashinfo.com{url}"
+    url = f"https://www.planecrashinfo.com{url}"
     with urlopen(url) as request:
         soup = BeautifulSoup(request, "lxml")
     raw_value = list(filter(lambda x: re.search(r"\d{4}", x),
